49d831545368000fcd5770748fd6f457bfd7eccb
Forest Katsch <me@forestkatsch.com>
2023-09-14 18:45:35 -0700

    Initial Commit

---
+++ Tangerine/TangerineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangerineApp.swift
+//  Tangerine
+//
+//  Created by Forest Katsch on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TangerineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TangerineUITests/TangerineUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TangerineUITestsLaunchTests.swift
+//  TangerineUITests
+//
+//  Created by Forest Katsch on 9/14/23.
+//
+
+import XCTest
+
+final class TangerineUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
